ccc50d2d22099d3788a5529b215c9193b13296f2
= <=>
2023-01-11 09:56:23 +0700

    Hoan thien phan Splash Screen

--- Source code/app.py
+++ Source code/app.py
@@ -1,11 +1,18 @@
-from view.handle import MainHandle
+from view.handle import MainHandle,SplashHandle
 from PyQt5 import QtWidgets, uic, QtCore
 from time import sleep
 import sys
 if __name__ == "__main__":
     MainApp = QtWidgets.QApplication(sys.argv)
+    screen_center = lambda widget: QtWidgets.QApplication.desktop().screen().rect().center()- widget.rect().center()
+
+    splash =SplashHandle.SplashScreen()
+    splash.show()
+    splash.move(screen_center(splash))
+    splash.progress()
     App = MainHandle.MainWindow()
     # print(type(App))
+    # App.move(screen_center(App))
     App.show()
-    # App.grab().save("image.png");
+    # splash.finish(App)
     sys.exit(MainApp.exec_())
--- Source code/view/handle/SplashHandle.py
+++ Source code/view/handle/SplashHandle.py
@@ -1,25 +1,19 @@
 import sys
 import time
 from PyQt5 import QtWidgets, uic
-class ThueXeQWidgets(QtWidgets.QWidget):
+class SplashScreen(QtWidgets.QSplashScreen):
     def __init__(self):
         super().__init__()
         uic.loadUi("./view/ui/splash.ui", self)
         self.progressBar.setValue(0)
-        self.frame.mousePressEvent=self.run
+        # self.frame.mousePressEvent=self.run
         # self.run()
 
-    def run(self,Event):
+    def progress(self):
         for i in range(100):
             time.sleep(0.05)
             self.progressBar.setValue(i+1)
+        # time.sleep(0.1)
+        # self.close()
         
     
-if __name__ == "__main__":
-    MainApp = QtWidgets.QApplication(sys.argv)
-    App = ThueXeQWidgets()
-    # print(App.__dict__)
-    App.show()
-    # App.run()
-    # MainApp.exit()
-    sys.exit(MainApp.exec_())
