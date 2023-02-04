1237fc3d91a5f1708e575d51164df45b490ccd82
= <=>
2023-02-04 21:48:06 +0700

    Them phan Phuong Thuc Tinh Toan cua Phong

--- Source code/control/phuongThucThueXe/BinhThuong.py
+++ Source code/control/phuongThucThueXe/BinhThuong.py
@@ -2,5 +2,12 @@ from control.phuongThucThueXe.TinhTienThueXe import TinhTienThueXe
 #Chưa code
 class BinhThuong(TinhTienThueXe):
     def tinhTien(self, thoiDiemDau, thoiDiemCuoi, k=1):
-        return 200000
         
+        count = thoiDiemCuoi - thoiDiemDau
+        count = count.days*24 * 60 +  int(  count.seconds / 60  )
+        if count <= 10 :
+            return 0
+        elif count <= 30 :
+            return 10000 * k
+        else :
+            return (10000 + ( int((count - 30 - 1 ) / 15) + 1 )  * 3000 ) * k
--- Source code/control/phuongThucThueXe/Goi24h.py
+++ Source code/control/phuongThucThueXe/Goi24h.py
@@ -2,4 +2,13 @@ from control.phuongThucThueXe.TinhTienThueXe import TinhTienThueXe
 #Chưa code
 class Goi24h(TinhTienThueXe):
     def tinhTien(self, thoiDiemDau, thoiDiemCuoi, k=1):
-        return 200000
+        count = thoiDiemCuoi - thoiDiemDau
+        count = count.days*24 * 60 +  int(  count.seconds / 60  )
+
+        if count >= 1440 :
+            return (200000 + (int( (count - 1 - 1440 ) / 15 ) + 1)* 2000 ) * k
+        elif count >= 720 :
+            return 200000 * k
+        else :
+            count = int( (count -1) / 60 ) + 1
+            return (200000 - (12- count) * 10000 ) * k
