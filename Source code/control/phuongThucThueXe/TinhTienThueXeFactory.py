51717821fa21093d3b8057b042f4029c8b890571
= <=>
2023-02-14 22:21:33 +0700

    Sua phan factory

--- Source code/control/phuongThucThueXe/TinhTienThueXeFactory.py
+++ Source code/control/phuongThucThueXe/TinhTienThueXeFactory.py
@@ -1,12 +1,9 @@
 from control.phuongThucThueXe import BinhThuong,Goi24h
 
 def getTinhTienThueXe(i):
-    
-    if i==0:
-        tinhTien= BinhThuong.BinhThuong()
-    elif i==1:
-        tinhTien= Goi24h.Goi24h()
-    else:
+    try:
+        return [BinhThuong.BinhThuong,Goi24h.Goi24h][i]()
+    except:
         raise Exception("Không tìm thấy phương thức tính tiền")
-    return tinhTien
+    
 
